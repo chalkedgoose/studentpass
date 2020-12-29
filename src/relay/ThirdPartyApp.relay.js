7db22a118a6567dfa31bc39cb46ae766ce7f4e95
Carlos Alba <soradev4@gmail.com>
2020-12-29 13:46:14 -0800

    ThirdPartyApp.relay

---
+++ src/relay/ThirdPartyApp.relay.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+// TODO(1) Implement ThirdPartyApp Relay
+
+/**
+ * This class will be used to communicate with 
+ * third party application clients who want to 
+ * authenticate with our service and access student 
+ * Identities.
+ */
+class ThirdPartyApp {
+    static sendExternalAccessToken(applicationURL, path = "/oauth-callback") {
+        // response = axios.post(applicationURL + path)
+        return
+    }
+}
